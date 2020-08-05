3d82cb4663da6ebbae5ce60fbc465e3fcbad9856
hortencia c <hortenciaecisneros@gmail.com>
2020-08-05 15:14:25 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -12,7 +12,8 @@ end
 
 # notes to self make sure you are wrtting everything as required , if asked !! then inculde wasted too much time on this !
 #  create a class for cat 
-# 
+# next create your attr_accessor to access your instance name ! 
+# define "meow" and "puts" it out = "meow!". end ,end 
 
 
 
